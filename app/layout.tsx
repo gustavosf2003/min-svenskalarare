f1d1fc4a8ee036bac72ecf9aa7c77353d0a105fe
Gustavo Ferreira <gustavo@queenslab.pt>
2023-11-10 01:20:31 +0000

    feat: google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Public_Sans } from "next/font/google";
 
 import { Navbar } from "@/components/Navbar";
 import Meta from "../components/Meta";
+import Script from "next/script";
 
 const publicSans = Public_Sans({ subsets: ["latin"] });
 
@@ -14,6 +15,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Meta />
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_KEY}`}
+        id="google-analytics"
+      />
+
+      <Script strategy="lazyOnload" id="google-analytics-config">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.GOOGLE_ANALYTICS_KEY}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
       <body className={publicSans.className}>
         <div className="flex flex-col p-4 md:p-12 h-[100vh]">
           <Navbar />
