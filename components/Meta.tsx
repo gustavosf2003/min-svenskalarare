23bf67a8f1ac8f9cb456efac097144e80c6deb87
GustavoQueenslab <gustavo@queenslab.pt>
2024-04-16 01:17:47 +0100

    fix: change meta description

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -5,19 +5,19 @@ export default function Meta() {
       <link rel="shortcut icon" href="/images/favicon.ico" />
       <meta
         name="description"
-        content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
+        content="Min Svenska Lärare - Din virtuella assistent för att lära dig svenska. Upptäck enkla sätt att förbättra din svenska med vår onlineassistent. Börja din språkresa idag!"
       />
       <meta property="og:title" content="Min svenska lärare" />
       <meta
         property="og:description"
-        content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
+        content="Min Svenska Lärare - Din virtuella assistent för att lära dig svenska. Upptäck enkla sätt att förbättra din svenska med vår onlineassistent. Börja din språkresa idag!"
       />
       <meta property="og:image" content="/images/og-image.png" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="Min svenska lärare" />
       <meta
         name="twitter:description"
-        content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
+        content="Min Svenska Lärare - Din virtuella assistent för att lära dig svenska. Upptäck enkla sätt att förbättra din svenska med vår onlineassistent. Börja din språkresa idag!"
       />
       <meta name="twitter:image" content="/images/og-image.png" />
     </head>
