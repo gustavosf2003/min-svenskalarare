ddde951b33cc5d97deadce61f29c7411a3a8b499
GustavoQueenslab <gustavo@queenslab.pt>
2024-07-31 00:29:45 +0100

    feat: switch funcionality

--- components/translator/index.tsx
+++ components/translator/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 
+import { ArrowsDownUp } from "@phosphor-icons/react";
 import { useQuery } from "@tanstack/react-query";
 
 import { useDebounce } from "@/hooks/useDebounce";
@@ -46,32 +47,38 @@ const TranslatorComponent = () => {
   const [selectedTargetLocale, setSelectedTargetLocale] = useState(
     languages[1],
   );
-
   const [text, setText] = useState("");
 
   const { data, refetch, isFetching, isError } = useQuery({
     queryKey: ["translator", text, selectedLocale, selectedTargetLocale],
-    queryFn: () => {
-      return translatorService.translate(
+    queryFn: () =>
+      translatorService.translate(
         text,
         selectedLocale.sourceLocale,
         selectedTargetLocale.targetLocale,
-      );
-    },
+      ),
     initialData: "",
-    enabled: false,
+    enabled: false, // Set to true to automatically fetch on mount
   });
+
   const debouncedSearch = useDebounce(text);
 
   useEffect(() => {
-    const loadUsers = async () => {
+    const loadTranslation = async () => {
       if (text.length > 0 && text.length < 400) {
         await refetch();
       }
     };
-    loadUsers();
+    loadTranslation();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [debouncedSearch, selectedLocale, selectedTargetLocale]);
+
+  const handleSwapLanguages = () => {
+    const temp = selectedLocale;
+    setSelectedLocale(selectedTargetLocale);
+    setSelectedTargetLocale(temp);
+    setText(data || "");
+  };
 
   return (
     <div className="justify-between w-full h-full gap-4 md:flex">
@@ -93,15 +100,23 @@ const TranslatorComponent = () => {
           onChange={(e) => setText(e.target.value)}
         />
       </div>
-      <div className="flex flex-col items-start w-full mt-3 gap-3 md:mt-0">
-        <Dropdown
-          items={languages.filter(
-            (language) =>
-              language.targetLocale !== selectedTargetLocale.targetLocale,
-          )}
-          selectedItem={selectedTargetLocale}
-          setSelectedItem={setSelectedTargetLocale}
-        />
+      <div className="flex flex-col items-start w-full gap-3 mt-3 md:mt-0">
+        <div className="flex justify-between w-full">
+          <Dropdown
+            items={languages.filter(
+              (language) =>
+                language.targetLocale !== selectedTargetLocale.targetLocale,
+            )}
+            selectedItem={selectedTargetLocale}
+            setSelectedItem={setSelectedTargetLocale}
+          />
+          <button
+            className="px-3.5 hover:bg-[#2F2F2F] hover:bg-opacity-40 hover:rounded-full"
+            onClick={handleSwapLanguages}
+          >
+            <ArrowsDownUp className="rotate-90" size={20} />
+          </button>
+        </div>
         <div className="bg-[#3E3D3B]/20 w-full h-40 md:h-full rounded-xl px-3.5 py-[10.5px] text-sm">
           {isFetching ? (
             <div>
