76bcdd8e2a5b2f1325e6a673360e13e07ad3c543
GustavoQueenslab <gustavo@queenslab.pt>
2024-08-06 23:56:40 +0100

    fix: dropdown bug

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -18,10 +18,10 @@ const Dropdown = ({ items, selectedItem, setSelectedItem }: DropdownProps) => {
             <ChevronUpDownIcon color="white" width={18}></ChevronUpDownIcon>
           </span>
         </Listbox.Button>
-        <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-[#2F2F2F] py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+        <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-[#2F2F2F] py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-20">
           {items.map((item) => (
             <Listbox.Option
-              className="flex items-center px-2 py-1 cursor-pointer hover:bg-basePrimary gap-2"
+              className="flex items-center gap-2 px-2 py-1 cursor-pointer hover:bg-basePrimary"
               key={item.icon}
               value={item}
             >
